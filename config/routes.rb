5d843eb8d5d880f4b51ae373acc9e529ba4e2d29
nvtuong <nvtuongcs@gmail.com>
2017-06-14 20:06:08 +0700

    milestone 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+
+  get 'photos/show'
+
+  resources :photos
+
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
